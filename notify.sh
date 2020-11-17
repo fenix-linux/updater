032ec5751a28ef489c73543f5bde4746a4020e79
fenixlinuxos <66138813+fenixlinuxos@users.noreply.github.com>
2020-11-17 15:29:09 +0100

    Update notify.sh

--- notify.sh
+++ notify.sh
@@ -5,19 +5,14 @@ while :
 do
 echo " -------------------------------------------------------------"
 echo "1. Comprobar actualizaciones "
-echo "2. Evitar que el sistema arranque de forma que los efectos y Cinnamon lentos."
-echo "3. Novedades fenix os"
+echo "2. Novedades fenix os"
 echo "q. Salir"
-echo -n "Seleccione una opción [1 - 4] "
+echo -n "Seleccione una opción [1 - 2] "
 echo " -------------------------------"
 read opcion
 case $opcion in
-1) echo "FenixPI 0.7 Ya está desponible.";;
-2)	sudo mv /home/no10/.notify/git/parche1.sh /usr/bin; sleep 1;
-	sudo chmod 755 /usr/bin/parche1.sh; sleep 1;
-	sudo xterm parche1.sh;
-	echo "Se ha aplicado el parche con éxito";;
-3) echo "Se ha lanzado FenixPI 0.7";;
+1) echo "FenixPI 0.8 Ya está desponible.";;
+2) echo "Se ha lanzado FenixPI 0.8";;
 q) echo -e "Gracias por usar el asistente, chao.\n
 Nota: Se irán añadiendo más funciones en cada versión.\n\n2020androrama ";
  sleep 3; exit 1;;
